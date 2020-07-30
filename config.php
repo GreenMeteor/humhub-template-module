d874a28bed5cd44c23139c2a79df9e84c3ab03fb
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-07-30 06:46:51 -0400

    Enh: Code cleanup

--- config.php
+++ config.php
@@ -1,23 +1,16 @@
 <?php
 
+use humhub\humhub\modules\template\Module;
 use humhub\humhub\modules\template\Events;
 use humhub\modules\admin\widgets\AdminMenu;
 use humhub\widgets\TopMenu;
 
 return [
 	'id' => 'template',
-	'class' => 'humhub\humhub\modules\template\Module',
+	'class' => Module::class,
 	'namespace' => 'humhub\humhub\modules\template',
 	'events' => [
-		[
-			'class' => TopMenu::class,
-			'event' => TopMenu::EVENT_INIT,
-			'callback' => [Events::class, 'onTopMenuInit'],
-		],
-		[
-			'class' => AdminMenu::class,
-			'event' => AdminMenu::EVENT_INIT,
-			'callback' => [Events::class, 'onAdminMenuInit']
-		],
+		['class' => TopMenu::class, 'event' => TopMenu::EVENT_INIT, 'callback' => [Events::class, 'onTopMenuInit'],],
+		['class' => AdminMenu::class, 'event' => AdminMenu::EVENT_INIT, 'callback' => [Events::class, 'onAdminMenuInit']],
 	],
 ];
